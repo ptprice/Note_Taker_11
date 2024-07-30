4c9190d6148d9c59d9addf2837cb18b9dde8fe40
Mariah Wear <mariahweardev@gmail.com>
2024-07-29 21:04:06 -0600

    debugging errors with routing and api calls

--- apiRoutes.js
+++ apiRoutes.js
@@ -1,20 +1,44 @@
 const router = require('express').Router();
+const util = require('util');
 const fs = require('fs');
 
-router.get('/notes', async (req, res) => {    
- const notes =await fs.readFile('/db/db.json'); 
- console.log("Notes",notes)
- notes.toString(); console.log(notes.toString());
-    res.json(notes);
-  
-    
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
+router.get('/notes', async (req, res) => {
+    readFileAsync('./db/db.json', 'utf8').then((notes) => {
+        let parsedNotes
+
+        try {
+            parsedNotes = [].concat(JSON.parse(notes))
+            console.log("parsednotes", parsedNotes)
+        } catch (err) {
+            parsedNotes = []
+        } return res.json(parsedNotes)
+
+    });
+
 })
-router.post('/notes', async(req, res) => {
- const existingNotes = await fs.readFile('/db/db.json');;
-    const newNote = req.body;
-    existingNotes.push(newNote);
-    await fs.writeFileSync('/db/db.json', JSON.stringify(existingNotes));   
+router.post('/notes', async (req, res) => {
+    // const existingNotes = await fs.readFile('/db/db.json');
+    const newNote = req.body
+    console.log('new note', newNote)
+
+    let existingNotes = readFileAsync('./db/db.json', 'utf8').then((notes) => {
+        let parsedNotes
+
+        try {
+            parsedNotes = [].concat(JSON.parse(notes))
+            console.log("parsednotes", parsedNotes)
+        } catch (err) {
+            parsedNotes = []
+        } return parsedNotes
+
+    }).then((notes) => [...notes, newNote]).then((updatedNotes) => writeFileAsync('./db/db.json', JSON.stringify(updatedNotes))).then(() => newNote)
+    console.log('existing notes', existingNotes)
+
     res.json(existingNotes);
-})      
+})
 
 module.exports = router;
